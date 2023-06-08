58b703d08bb0f5fcbc52837e227759892ec9a102
aleksei <example@gmail.com>
2023-06-08 19:24:45 +0300

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import styles from "../styles/Header.module.css"
 
 import  Logo from "../../images/logo.svg"
 // import  Avatar from "../../images/avatar.jpg"
-import loop from "../../images/loop.png"
+import basket from "../../images/basket.png"
 
 
 const Header = () => {
@@ -18,22 +18,12 @@ const Header = () => {
         </Link>
       </div>
 
-      <div className={styles.info}>
-        <div className={styles.user}>
-          <div className={styles.avatar} />
-          <div className={styles.username}>Guest</div>
-      </div>
-
       <form className={styles.form}>
-        <div className={styles.icon}>
-          <img className="icon" src={loop}/>
-        </div>
-
         <div className={styles.input}>
             <input 
               type='search' 
               name='search' 
-              placeholder='Searching..'
+              placeholder='Search...'
               autoComplete='off'
               onChange={() => {}}
               value=""
@@ -43,13 +33,15 @@ const Header = () => {
           {false && <div className={styles.box}></div>}
       </form>
 
-      <div className={styles.account}>
-        <Link to={ROUTES.HOME} className={styles.favourites}>
-          <img className="icon" src={loop}/>
-        </Link>
+      <div className={styles.info}>
+        <div className={styles.user}>
+          <div className={styles.avatar} />
+          <div className={styles.username}>Guest</div>
+      </div>
 
+      <div className={styles.account}>
         <Link to={ROUTES.CART} className={styles.favourites}>
-          <img className="icon" src={loop}/>
+          <img className="icon" src={basket}/>
         </Link>
         {/* <span className={styles.count}>2</span> */}
       </div>
